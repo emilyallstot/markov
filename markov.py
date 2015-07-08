daf66c71dc47fe2fdaaa79f73728b149dc4890b8
Hackbright Student <info@hackbrightacademy.com>
2015-07-07 17:42:14 -0700

    Say Hello

--- markov.py
+++ markov.py
@@ -1,5 +1,54 @@
 import sys
 
+# note: does not handle wrapping first and last word of file for markov chains
+
+def make_values(corpus, word_key):
+    
+    word_key = tuple(word_key)
+
+    values_list = []
+    
+    file = open(corpus)
+
+    lastword = ""
+    last_key = ()
+
+    for line in file:
+        line = line.strip()
+        words = line.split(" ")
+        
+
+        if last_key == word_key:
+            values_list.append(words[0])
+
+
+        if word_key[1] == words[0]:
+        
+            if word_key[0] == lastword:
+                print word_key
+                values_list.append(words[1])
+                
+    
+        
+        i = 0
+        while i < len(words) - 2:
+            if word_key[0] == words[i]:
+                if word_key[1] == words[i + 1]:
+                    values_list.append(words[i + 2])
+            i += 1
+
+
+
+        last_key = tuple(words[-2:])
+        lastword = words[-1]
+
+    return values_list
+
+print('***')*33
+# print make_values("green-eggs.txt", ('you', 'like'))
+print make_values("green-eggs.txt", ('a', 'mouse?'))
+print('****')*33
+
 
 def make_chains(corpus):
     """Takes input text as string; returns dictionary of markov chains."""
@@ -19,15 +68,30 @@ def make_chains(corpus):
         word_key[0] = words[len(words)-1]
         word_key[1] = words[0]
         
-        #markov_dictionary[tuple(word_key)] = make_values(corpus, word_key)
-        markov_dictionary[tuple(word_key)] = []
+        markov_dictionary[tuple(word_key)] = make_values(corpus, word_key)
+
+        # tuple_key = tuple(word_key)
+
+        # markov_dictionary[tuple_key] = markov_dictionary[tuple_key].append(make_values(corpus, word_key))
+
+        # markov_dictionary =  markov_dictionary + {markov_dictionary[tuple_key]:make_values(corpus, word_key)}
+        # markov_dictionary.update({tuple_key: make_values(corpus, word_key)})
+        #markov_dictionary[tuple(word_key)] = []
 
         i = 0
         while i < len(words) - 1:
             word_key[0] = words[i]
             word_key[1] = words[i + 1]
-            #markov_dictionary[tuple(word_key)] = make_values(corpus, word_key)
-            markov_dictionary[tuple(word_key)] = []
+            # tuple_key = tuple(word_key)
+            
+            # markov_dictionary[tuple_key] = markov_dictionary[tuple_key].append(make_values(corpus, word_key))
+
+
+            # markov_dictionary =  markov_dictionary + {markov_dictionary[tuple_key]:make_values(corpus, word_key)}
+      
+            #markov_dictionary.update({tuple_key: make_values(corpus, word_key)})
+            markov_dictionary[tuple(word_key)] = make_values(corpus, word_key)
+            #markov_dictionary[tuple(word_key)] = []
 
             i += 1
     return markov_dictionary
@@ -35,29 +99,7 @@ def make_chains(corpus):
 print make_chains("green-eggs.txt")
 
 
-def make_values(corpus, word_key):
-    
-    values_list = []
-    
-    file = open(corpus)
 
-    for line in file:
-        line = line.strip()
-        words = line.split(" ")
-        
-        i = 0
-        while i < len(words) - 2:
-            if word_key[0] == words[i]:
-                if word_key[1] == words[i + 1]:
-                    values_list.append(words[i + 2])
-            
-
-            i += 1
-
-    return values_list
-
-print('\n')
-print make_values("green-eggs.txt", ('you', 'like'))
 
 
 # def make_text(chains):
